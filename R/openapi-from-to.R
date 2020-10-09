38d7e2c815af5909d89a001fac6ba8c3f53677cf
Bruno Tremblay <bruno.tremblay@lacapitale.com>
2020-10-09 08:20:03 -0400

    in progress

--- R/openapi-from-to.R
+++ R/openapi-from-to.R
@@ -1,76 +1,77 @@
-#' Convert router to/from OpenAPI specifications
 #' @rdname openapi
-#' @param pr A plumber router or a plumbable file or directory.
-#' @description These functions are used to convert between OpenAPI
-#' Specifications and plumber file. Plumber only supports a limited
-#' set of OpenAPI Specifications.
-#' @param output An optional filename where to write OpenAPI Specifications.
-#' @param format Either `json` or `yaml`.
-#' @details OpenAPI Specifications are used to document APIs. More info
-#' can be found at (https://www.openapis.org/)
-#' @examples
-#' pr <- plumber$new()
-#' to_openapi(pr)
+#' @param openapi a URL or file path to an OpenAPI document
 #' @export
-to_openapi <- function(plumber, output = NULL) {
-  if (!inherits(pr, "plumber")) {
-    if (file.exists(plumber)) {
-      if (file.info(plumber)$isdir) {
-        pr <- plumb(dir = plumber)
-      } else {
-        pr <- plumb(plumber)
-      }
-    }
-  }
-  spec <- pr$apiSpec()
-  openapi_url <- {
-    api_url_1 <- getOption("plumber.apiURL")
-    api_url_2 <- urlHost(scheme = getOption("plumber.apiScheme", ""),
-                         host   = getOption("plumber.apiHost", ""),
-                         port   = getOption("plumber.apiPort", ""),
-                         path   = getOption("plumber.apiPath", ""),
-                         changeHostLocation = TRUE)
-    priorizeProperty(api_url_1, api_url_2)
-  }
-
-  spec$servers$url <- openapi_url
-
-  spec <- jsonlite::toJSON(spec, auto_unbox = TRUE)
-  if (!is.null(output)) {
-    writeLines(spec, output)
-    return()
-  }
-  return(spec)
-}
-
-#' @rdname openapi
-#' @param openapi an OpenAPI specifications string, URL or file
-#' @param format Input file format. Either "json" or "yml". If not
-#' provided, will be guessed from file extension.
-#' @export
-from_openapi <- function(openapi, format = c("json", "yaml")) {
-  format <- match.arg(format)
-  spec <- new.env()
-  if (tools::file_ext(openapi) %in% c("yml", "yaml") || format == "yaml") {
-    if (!requireNamespace("yaml", quietly = TRUE)) {
-      stop("The yaml package is not available but is required in order to parse yaml specifications.\ninstall.packages(\"yaml\")",
-           call. = FALSE)
-    }
-    if (stringi::stri_detect_fixed(openapi, "\n")) {
-      s <- yaml::yaml.load(openapi)
-    } else {
-      s <- yaml::read_yaml(openapi)
-    }
-  } else {
-  s <- jsonlite::fromJSON(openapi)
-  }
-  mapply(assign, names(s), s, MoreArgs = list(envir = spec))
-  stubSpec(spec)
-  return(spec)
+from_openapi <- function(openapi) {
+  s <- resolve_spec(openapi)
+  lines <- stub_spec(s)
+  return(lines)
 }
 
 #' @noRd
-stubSpec <- function(spec) {
+resolve_uri <- function(uri) {
+  # Regex adapted from https://tools.ietf.org/html/rfc3986#appendix-B
+  ref <- stri_match_first_regex(uri, "^(?:([^:/?#]+):)?(?://([^/?#]*))?([^?#]*)(?:\\?([^#]*))?(?:#(.*))?")
+  colnames(ref) <- c("origin", "scheme", "authority", "path", "query", "fragment")
+
+  url <- character(length(uri))
+
+  idx <- !is.na(ref[, "scheme"])
+  url[idx] <- paste0(url[idx], ref[idx, "scheme"], ":")
+
+  idx <- !is.na(ref[, "authority"])
+  url[idx] <- paste0(url[idx], "//", ref[idx, "authority"])
+
+  idx <- !is.na(ref[, "path"])
+  url[idx] <- paste0(url[idx], ref[idx, "path"])
+
+  domain <- dirname(url)
+
+  idx <- !is.na(ref[, "query"])
+  url[idx] <- paste0(url[idx], "?", ref[idx, "query"])
+
+  url[nchar(url) == 0L] <- NA_character_
+
+  cbind(ref, url)
+
+}
+
+#' @noRd
+resolve_spec <- function(openapi, env = new.env()) {
+
+  stopifnot(length(openapi) == 1L)
+  uri <- resolve_uri(openapi)
+  if (!is.na(uri[, "url"])) {
+    if (tools::file_ext(uri[, "path"]) %in% c("yml", "yaml")) {
+      if (!requireNamespace("yaml", quietly = TRUE)) {
+        stop("The yaml package is not available but is required in order to parse yaml documentation\ninstall.packages(\"yaml\")",
+             call. = FALSE)
+      }
+      s <- yaml::read_yaml(uri[, "url"])
+    } else {
+      s <- jsonlite::fromJSON(uri[, "url"])
+    }
+    mapply(assign, names(s), s, MoreArgs = list(envir = env))
+    assign(".__source__.")
+  }
+
+  fragment <- "#/components/schemas/Weather/properties/description"
+  eval(parse(text = paste(c("spec", stri_split_fixed(stri_replace_first_regex(fragment, "^#/?", ""), "/")[[1]]),collapse = "$"))))
+
+
+  if (!is.na(uri[, "fragment"])) {
+    resolve_ref()
+  }
+
+  resolve_ref(env)
+}
+
+#' @noRd
+resolve_ref <- function(env) {
+
+}
+
+#' @noRd
+stub_spec <- function(spec) {
 
   info_lines <- function(lines, value, field) {
     if (is.null(value)) return(lines)
